f57e024cc0d2c147af4dae7bada43386c35e9c4d
Furkan Doğu <furkandogu2018@gmail.com>
2024-01-28 15:37:21 +0300

    addtodo bitti

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { Box, Button, TextField } from '@mui/material'
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import { useState } from 'react';
 
-const AddTodo = () => {
+interface IAddTodo {
+  todoAdd:AddFunc
+}
+
+const AddTodo = ({todoAdd}:IAddTodo) => {
+  const [text, setText] = useState("")
+
+  const handleClick = () => {
+    todoAdd(text)
+    setText("")
+  }
   return (
-    <div>AddTodo</div>
+    <Box
+        sx={{
+          display: { xs: "block", sm: "flex" },
+          justifyContent: { xs: "flex-start", sm: "center" },
+          m: { xs: 1, sm: "auto" },
+          height: { xs: "120px", sm: "80px" },
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="New Todo"
+          color="success"
+          sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+          variant="outlined"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="success"
+          sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
+          disabled={!text.trim()}
+          startIcon={<ListAltIcon />}
+          onClick={handleClick}
+        >
+          add
+        </Button>
+      </Box>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import AddTodo from "../components/AddTodo";
 import TodoList from "../components/TodoList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 interface ITodoType {
@@ -12,19 +12,43 @@ interface ITodoType {
   todo?: string;
 }
 
+const url: string = import.meta.env.VITE_BASE_URL;
+
 const Home = () => {
-  const [todo, SetTodo] = useState([]);
+  const [todo, setTodo] = useState<ITodoType[]>([]);
 
   const getTodos = async () => {
     try {
-      const { data } = await axios("");
-    } catch (error) {}
+      const { data } = await axios.get<ITodoType[]>(url);
+      setTodo(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+//   type AddFunc = (text:string) => Promise<void>
+
+  const todoAdd:AddFunc = async (text) => {
+    try {
+        await axios.post(url, {task:text, isDone:false})
+    } catch (error) {
+        console.log(error);
+    } finally {
+        getTodos()
+    }
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return (
     <Container>
-      <Typography>TodoApp with TypeScript</Typography>
-      <AddTodo />
+      <Typography color={"error"} align="center" variant="h2" component={"h1"}>
+        TodoApp with TypeScript
+      </Typography>
+      <AddTodo todoAdd={todoAdd} />
       <TodoList />
     </Container>
   );
